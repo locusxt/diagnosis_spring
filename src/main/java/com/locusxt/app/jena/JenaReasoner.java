1ede4fd70994b4e144078b87ac77b93bdbce0926
locusxt <locusxt@163.com>
2015-08-03 15:29:22 +0800

    add case ontology generator

---
+++ src/main/java/com/locusxt/app/jena/JenaReasoner.java
@@ -0,0 +1,51 @@
+package com.locusxt.app.jena;
+
+import com.hp.hpl.jena.ontology.DatatypeProperty;
+import com.hp.hpl.jena.ontology.ObjectProperty;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.locusxt.app.domain.PatientInfo;
+
+public class JenaReasoner {
+	public static String defaultNameSpace="http://somewhere/";
+	public static OntModel mData;
+	
+	public void genDatamodel(PatientInfo info){
+		OntClass ont = mData.createClass(defaultNameSpace + info.getName());
+		int complaintNum = info.getComplaint().length;
+		OntClass complaint[] = new OntClass[complaintNum];
+		
+		ObjectProperty hasSymptomOf = mData.createObjectProperty(defaultNameSpace + "hassymptomof");
+		ObjectProperty hasDegreeOf = mData.createObjectProperty(defaultNameSpace + "has_degree_of");
+		DatatypeProperty hasDurationDay = mData.createDatatypeProperty(defaultNameSpace + "has_duration_day");
+		
+		for (int i = 0; i < complaintNum; ++i){
+			complaint[i] = mData.createClass(defaultNameSpace + info.getComplaint()[i]);
+			mData.add(ont, hasSymptomOf, complaint[i]);
+			if (!info.getComplaintDegree()[i].equals("unselected")){
+				mData.addLiteral(complaint[i], hasDegreeOf, info.getComplaintDegree()[i]);
+			}
+			if (!info.getComplaintTime()[i].equals("")){
+				mData.addLiteral(complaint[i], hasDurationDay, info.getComplaintTime()[i]);
+			}
+		}
+		
+		int phyExamNum = info.getPhyExam().length;
+		OntClass phyExam[] = new OntClass[phyExamNum];
+		DatatypeProperty phyExamProperty[] = new DatatypeProperty[phyExamNum];
+		for (int i = 0; i < phyExamNum; ++i){
+			phyExam[i] = mData.createClass(defaultNameSpace + info.getPhyExam()[i]);
+			phyExamProperty[i] = mData.createDatatypeProperty(defaultNameSpace + "has_test_" + info.getPhyExam()[i]);
+			mData.addLiteral(ont, phyExamProperty[i], info.getPhyExamResult()[i]);
+		}
+		
+		int testNum = info.getTest().length;
+		OntClass test[] = new OntClass[testNum];
+		DatatypeProperty testProperty[] = new DatatypeProperty[testNum];
+		for (int i = 0; i < testNum; ++i){
+			test[i] = mData.createClass(defaultNameSpace + info.getTest()[i]);
+			testProperty[i] = mData.createDatatypeProperty(defaultNameSpace + "has_test_" + info.getTest()[i]);
+			mData.addLiteral(ont, testProperty[i], info.getTestResult()[i]);
+		}
+	}
+}
